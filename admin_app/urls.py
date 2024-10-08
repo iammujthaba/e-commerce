6847b1a66b58d3a19dfb5c00ad74bace0b489cda
mujthaba <iammujthaba@gmail.com>
2024-10-08 11:11:25 +0530

    admin_app/urls.py file URLs order arrainged.

--- admin_app/urls.py
+++ admin_app/urls.py
@@ -9,20 +9,24 @@ urlpatterns = [
     path('admin_app/categories/create/', views.category_create, name='category_create'),
     path('admin_app/categories/update/<int:pk>/', views.category_update, name='category_update'),
     path('admin_app/categories/delete/<int:pk>/', views.category_delete, name='category_delete'),
+    path('admin_app/categories/update-priority/', views.update_category_priority, name='update_category_priority'),
+
     path('admin_app/products/', views.product_list, name='product_list'),
     path('admin_app/products/create/', views.product_create, name='product_create'),
     path('admin_app/products/update/<int:pk>/', views.product_update, name='product_update'),
-    # path('admin_app/products/delete/<int:pk>/', views.product_delete, name='product_delete'),
+    path('products/products/update-priority/', views.update_product_priority, name='update_product_priority'),
+
     path('admin_app/orders/pending/<int:pk>/', views.order_view, name='order_view'),
     path('admin_app/orders/processing/', views.processing_orders, name='processing_orders'),
     path('admin_app/orders/confirmed/', views.confirmed_orders, name='confirmed_orders'),
     path('admin_app/orders/shipped/', views.shipped_orders, name='shipped_orders'),
     path('admin_app/orders/completed/', views.completed_orders, name='completed_orders'),
-    path('admin_app/users/', views.users_list, name='users_list'),
-    path('products/products/update-priority/', views.update_product_priority, name='update_product_priority'),
-    path('admin_app/categories/update-priority/', views.update_category_priority, name='update_category_priority'),
+
     path('admin_app/shipping-rates/', views.shipping_rate_list, name='shipping_rate_list'),
     path('admin_app/shipping-rates/create/', views.shipping_rate_create, name='shipping_rate_create'),
     path('admin_app/shipping-rates/<int:pk>/update/', views.shipping_rate_update, name='shipping_rate_update'),
     path('admin_app/shipping-rates/<int:pk>/delete/', views.shipping_rate_delete, name='shipping_rate_delete'),
+
+    path('admin_app/users/', views.users_list, name='users_list'),
+    # path('admin_app/products/delete/<int:pk>/', views.product_delete, name='product_delete'),
 ]
