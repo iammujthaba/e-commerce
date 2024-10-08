7affae1aff53866924a8022fa6e6315e3b08151e
mujthaba <iammujthaba@gmail.com>
2024-10-08 11:15:50 +0530

    auth_app/views.py file function order arrainged.

--- auth_app/views.py
+++ auth_app/views.py
@@ -2,7 +2,6 @@ from django import forms
 from django.contrib import messages,auth
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
-
 from store.models import Product, Order, OrderItem, Customer, Wishlist
 from .forms import RegistrationForm, LoginForm, SuperuserPasswordForm
 from django.contrib.auth import authenticate, login as auth_login
@@ -12,7 +11,97 @@ import json
 SUPERUSER_CONTACT_NUMBER = '1234567890'
 PREDEFINED_SUPERUSER_PASSWORD = '1234'
 
-# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            contact_number = form.cleaned_data['contact_number']
+
+            user = User.objects.create_user(username=username)
+            Customer.objects.create(user=user, name=username, contact_number=contact_number)
+
+            # Log in the newly registered user
+            auth_login(request, user)
+
+            # Merge cookies cart with user cart
+            cart_response = merge_cookie_cart_with_user_cart(request, user)
+
+            # Merge cookies wishlist with user wishlist
+            wishlist_response = merge_cookie_wishlist_with_user_wishlist(request, user)
+
+            if cart_response:
+                return cart_response
+            elif wishlist_response:
+                return wishlist_response
+            return redirect('auth_app:login')
+        else:
+            for error in form.errors.values():
+                messages.error(request, error)
+            return redirect('auth_app:register')
+    form = RegistrationForm()
+    return render(request, 'register.html', {'form': form})
+
+
+# Add the merge_cookie_cart_with_user_cart function call in your login view
+def login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            contact_number = form.cleaned_data['contact_number']
+            if contact_number == SUPERUSER_CONTACT_NUMBER:
+                return redirect('auth_app:superuser_password')
+            else:
+                try:
+                    customer = Customer.objects.get(contact_number=contact_number)
+                    user = customer.user
+                    auth_login(request, user)
+                    
+                    # Merge cookie cart with user cart
+                    cart_response = merge_cookie_cart_with_user_cart(request, user)
+                    
+                    # Merge cookie wishlist with user wishlist
+                    wishlist_response = merge_cookie_wishlist_with_user_wishlist(request, user)
+                    
+                    # Determine which response to return
+                    if cart_response:
+                        return cart_response
+                    elif wishlist_response:
+                        return wishlist_response
+                    else:
+                        return redirect('/')
+                except Customer.DoesNotExist:
+                    form.add_error('contact_number', 'This number is not found')
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
+
+
+def superuser_password(request):
+    if request.method == 'POST':
+        form = SuperuserPasswordForm(request.POST)
+        if form.is_valid():
+            password = form.cleaned_data['password']
+
+            if password == PREDEFINED_SUPERUSER_PASSWORD:
+                # Authenticate superuser
+                superuser = Customer.objects.get(contact_number=SUPERUSER_CONTACT_NUMBER).user
+                auth_login(request, superuser)
+                return redirect('/')
+            else:
+                form.add_error('password', 'Incorrect superuser password')
+    else:
+        form = SuperuserPasswordForm()
+
+    return render(request, 'superuser_password.html', {'form': form})
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+
+
 def merge_cookie_cart_with_user_cart(request, user):
     try:
         cart = json.loads(request.COOKIES.get('cart', '{}'))
@@ -84,6 +173,7 @@ def merge_cookie_cart_with_user_cart(request, user):
 
         return None
 
+
 def merge_cookie_wishlist_with_user_wishlist(request, user):
     try:
         cookie_wishlist = json.loads(request.COOKIES.get('wishlist', '{}'))
@@ -101,92 +191,6 @@ def merge_cookie_wishlist_with_user_wishlist(request, user):
     response.delete_cookie('wishlist')
     return response
 
-
-# Add the merge_cookie_cart_with_user_cart function call in your login view
-def login(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            contact_number = form.cleaned_data['contact_number']
-            if contact_number == SUPERUSER_CONTACT_NUMBER:
-                return redirect('auth_app:superuser_password')
-            else:
-                try:
-                    customer = Customer.objects.get(contact_number=contact_number)
-                    user = customer.user
-                    auth_login(request, user)
-                    
-                    # Merge cookie cart with user cart
-                    cart_response = merge_cookie_cart_with_user_cart(request, user)
-                    
-                    # Merge cookie wishlist with user wishlist
-                    wishlist_response = merge_cookie_wishlist_with_user_wishlist(request, user)
-                    
-                    # Determine which response to return
-                    if cart_response:
-                        return cart_response
-                    elif wishlist_response:
-                        return wishlist_response
-                    else:
-                        return redirect('/')
-                except Customer.DoesNotExist:
-                    form.add_error('contact_number', 'This number is not found')
-    else:
-        form = LoginForm()
-    return render(request, 'login.html', {'form': form})
-
-def superuser_password(request):
-    if request.method == 'POST':
-        form = SuperuserPasswordForm(request.POST)
-        if form.is_valid():
-            password = form.cleaned_data['password']
-
-            if password == PREDEFINED_SUPERUSER_PASSWORD:
-                # Authenticate superuser
-                superuser = Customer.objects.get(contact_number=SUPERUSER_CONTACT_NUMBER).user
-                auth_login(request, superuser)
-                return redirect('/')
-            else:
-                form.add_error('password', 'Incorrect superuser password')
-    else:
-        form = SuperuserPasswordForm()
-
-    return render(request, 'superuser_password.html', {'form': form})
-
-def register(request):
-    if request.method == 'POST':
-        form = RegistrationForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            contact_number = form.cleaned_data['contact_number']
-
-            user = User.objects.create_user(username=username)
-            Customer.objects.create(user=user, name=username, contact_number=contact_number)
-
-            # Log in the newly registered user
-            auth_login(request, user)
-
-            # Merge cookies cart with user cart
-            cart_response = merge_cookie_cart_with_user_cart(request, user)
-
-            # Merge cookies wishlist with user wishlist
-            wishlist_response = merge_cookie_wishlist_with_user_wishlist(request, user)
-
-            if cart_response:
-                return cart_response
-            elif wishlist_response:
-                return wishlist_response
-            return redirect('auth_app:login')
-        else:
-            for error in form.errors.values():
-                messages.error(request, error)
-            return redirect('auth_app:register')
-    form = RegistrationForm()
-    return render(request, 'register.html', {'form': form})
-
-def logout(request):
-    auth.logout(request)
-    return redirect('/')
 
 def loginOrRegister(request):
     return render(request, 'loginOrRegister.html')
