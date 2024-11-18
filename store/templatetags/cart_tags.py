9eabc94e9a14c9030f637914d1bce6518a9988ca
mujthaba <iammujthaba@gmail.com>
2024-11-18 13:08:20 +0530

    button udated, still working on it.

--- store/templatetags/cart_tags.py
+++ store/templatetags/cart_tags.py
@@ -6,8 +6,10 @@ register = template.Library()
 @register.inclusion_tag('store/includes/cart_button.html', takes_context=True)
 def render_cart_button(context, product):
     request = context['request']
+    template_name = context.get('request').resolver_match.url_name
     return {
         'product': product,
         'is_in_cart': product.is_in_cart(request),
-        'cart_url': reverse('store_app:cart')
+        'cart_url': reverse('store_app:cart'),
+        'template_name': template_name
     }
