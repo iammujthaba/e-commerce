79d1d1f497ea8daf04e0d9325b330484603d1c9c
mujthabamk2 <mujthabamk2@gmail.com>
2025-07-04 12:36:06 +0530

    sql database code added on settings, and store/migrations/0011_*.py file on editing.

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -114,15 +114,15 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
 
-        # 'ENGINE': 'django.db.backends.mysql',
-        # 'NAME': 'guppies',
-        # 'PORT':'3306',
-        # 'HOST':'localhost',
-        # 'USER':'root',
-        # 'PASSWORD':'1234',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'guppies',
+        'PORT':'3306',
+        'HOST':'localhost',
+        'USER':'root',
+        'PASSWORD':'root',
     }
 }
 
